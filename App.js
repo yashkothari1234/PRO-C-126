db51421bac97dbceb2be41341a03a764c2c07fef
yashkothari1234 <68227727+yashkothari1234@users.noreply.github.com>
2021-07-10 12:44:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PickImage from "./screens/camera.js";
+
+export default class App extends React.Component() {
+  render(){
+    return <PickImage/>
+  }
+  
+}
+
+
